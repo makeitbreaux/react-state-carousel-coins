a1cc22bd888c6efe7986afe8992f47c94b037f2d
Briana Breaux <makeitbreaux@gmail.com>
2022-01-03 19:01:40 -0600

    Completed exercise for 39.5

---
+++ carousel/src/Card.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Card from "./Card";
+
+test("renders without crashing", function() {
+  render(<Card />);
+});
+
+test("matches snapshot", function() {
+  const { asFragment } = render(<Card />);
+  expect(asFragment()).toMatchSnapshot();
+});
